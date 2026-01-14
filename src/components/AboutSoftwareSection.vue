00c2e8b075542c1d35733df7b72c9e8678b3bf79
loic <allouyao21@gmail.com>
2026-01-14 10:50:14 +0000

    Ajout de details sur les demonstrations

--- src/components/AboutSoftwareSection.vue
+++ src/components/AboutSoftwareSection.vue
@@ -1,34 +1,39 @@
-
 <template>
   <section class="py-20 bg-white">
     <div class="max-w-5xl mx-auto px-6 text-center">
       <h2 class="text-3xl md:text-4xl font-bold text-primaryPurple mb-6">
-        Clinique NUMERISK, la clinique entre dans l’ère du numérique
+        Avec Clinique NUMERISK, entrez dans une nouvelle ère de gestion médicale digitale,
+        pensée pour la performance,
+        la sécurité et la qualité des soins.
       </h2>
 
       <p class="text-gray-600 text-lg leading-relaxed mb-8">
         <strong>
-          l’entreprise
+          L’entreprise
           <a href="https://numerisk.net" class="text-primaryOrange hover:underline" target="_blank" rel="noopener">
             NUMERISK
           </a>
-        </strong>, spécialisée dans la transformation digitale, propose des services de management, sécurité,
-        conception, développement et maintenance de logiciels, ainsi que l'installation et la maintenance de systèmes de
-        vidéosurveillance, de réseaux informatiques et de solutions de sécurité informatique.
-      </p>
-
-
-      <p class="text-gray-600 text-lg leading-relaxed mt-4">
-        Le logiciel a été pensé pour moderniser la gestion des cliniques,
-        améliorer la qualité des soins et optimiser le travail du personnel
-        médical et administratif grâce à une interface intuitive, rapide et
-        sécurisée.
+        </strong>, spécialisée dans la transformation digitale, accompagne les organisations dans leurs projets de
+        management, de sécurité, de conception, de développement et de maintenance de logiciels. Elle intervient
+        également
+        dans l'installation et la maintenance de systèmes de vidéosurveillance, de réseaux informatiques et de solutions
+        de
+        sécurité informatique.
       </p>
 
       <p class="text-gray-600 text-lg leading-relaxed mt-4">
-        Avec Clinique NUMERISK, les établissements de santé se concentrent sur
-        l’essentiel : <strong>le bien-être des patients</strong>.
+        Dans cette dynamique d’innovation, <strong>Clinique NUMERISK</strong> a été conçu pour moderniser la gestion des
+        cliniques, améliorer la qualité des soins et optimiser le travail du personnel médical et administratif grâce à
+        une
+        interface intuitive, rapide et sécurisée.
       </p>
+
+      <p class="text-gray-600 text-lg leading-relaxed mt-4">
+        Avec <strong>Clinique NUMERISK</strong>, les établissements de santé peuvent enfin se 
+        concentrer sur l’essentiel :
+        <strong class="text-primaryPurple">le bien-être et la satisfaction des patients</strong>.
+      </p>
+
     </div>
   </section>
 </template>
